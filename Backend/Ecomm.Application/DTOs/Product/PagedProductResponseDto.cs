07ef76b2e1e94837635e092e87584eb52eb0f5db
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-04-30 20:29:17 +0545

    add has next page and hasPreviousPage

--- Backend/Ecomm.Application/DTOs/Product/PagedProductResponseDto.cs
+++ Backend/Ecomm.Application/DTOs/Product/PagedProductResponseDto.cs
@@ -2,9 +2,12 @@
 
 public class PagedProductResponseDto
 {
-    public IEnumerable<ProductResponseDto> Items { get; set; } = Enumerable.Empty<ProductResponseDto>();
+    public IEnumerable<ProductResponseDto> Items { get; set; } = [];
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
 }
