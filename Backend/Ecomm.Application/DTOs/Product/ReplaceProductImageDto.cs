11008c652d9ac219959c1371bb4ccccbd80cb3fd
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-07 14:18:57 +0545

    add update image service

---
+++ Backend/Ecomm.Application/DTOs/Product/ReplaceProductImageDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ecomm.Application.DTOs.Product;
+
+public class ReplaceProductImageDto
+{
+    public IFormFile File { get; set; } = null!;  // ← Required!
+    public bool IsPrimary { get; set; } = false;  // ← Optional, default false
+}
