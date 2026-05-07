89e4c53940d9f89c43485c578023c258af2d4720
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-07 16:14:26 +0545

    add description

--- Backend/Ecomm.Application/DTOs/Category/CategoryResponseDto.cs
+++ Backend/Ecomm.Application/DTOs/Category/CategoryResponseDto.cs
@@ -5,4 +5,5 @@ public class CategoryResponseDto
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
+    public int ProductCount { get; set; } = 0;
 }
