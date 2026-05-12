5de333170d23889f407eed343df365edd5ca7c62
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-12 10:55:55 +0545

    add validation rule for discount price

--- Backend/Ecomm.Application/Validators/Product/ProductImportRowValidator.cs
+++ Backend/Ecomm.Application/Validators/Product/ProductImportRowValidator.cs
@@ -10,6 +10,11 @@ public class ProductImportRowValidator : AbstractValidator<ProductImportRowDto>
         RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
         RuleFor(x => x.SKU).NotEmpty().MaximumLength(100);
         RuleFor(x => x.Price).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.DiscountPrice)
+            .Must((dto, discount) =>
+                !discount.HasValue ||
+                (discount >= 0 && discount < dto.Price))
+            .WithMessage("Discount price must be between 0 and less than Price.");
         RuleFor(x => x.QuantityInStock).GreaterThanOrEqualTo(0);
         RuleFor(x => x.ReorderLevel).GreaterThanOrEqualTo(0);
         RuleFor(x => x.CategoryName).NotEmpty().MaximumLength(100);
